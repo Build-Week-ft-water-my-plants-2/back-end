3a9567ee724a538d667b035d6dfcb4a09a18b1da
DanielReinagel <daniel.e.reinagel@gmail.com>
2021-11-15 19:21:37 -0700

    POST /api/users/signup and /api/users/login are working properly

--- api/server.js
+++ api/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 
+const usersRouter = require("./users/users-router");
+
 const server = express();
 
 server.use(express.json());
+
+server.use("/api/users", usersRouter);
 
 server.use((req, res) => res.status(404).json({message:"Resource not found", method:req.method, path:req.url}));
 
